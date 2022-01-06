72543873c0cae55aff667998c4b4d670040a438f
shema102 <sergeishemshur@gmail.com>
2022-01-06 18:12:11 +0200

    removed unused configs

--- shell/external.sh
+++ shell/external.sh
@@ -27,7 +27,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
 export VIRTUALENVWRAPPER_PYTHON=$(which python3)
 export VIRTUALENVWRAPPER_VIRTUALENV=$(which virtualenv)
-source $(which virtualenvwrapper.sh)
+#source $(which virtualenvwrapper.sh)
 
 # Export Android Studio folder
 export PATH="$PATH:/opt/android-studio/bin"
