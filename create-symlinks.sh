abd56694f1e437beec87764205936f6de59046c4
shema102 <sergeishemshur@gmail.com>
2023-07-30 15:08:51 +0300

    add neovim config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,44 +2,12 @@
 
 DOTFILES_DIR=$(pwd)
 
-#==============
-# Create symlinks in the home folder
-# Allow overriding with files of matching names in the custom-configs dir
-#==============
-mkdir -p ~/.config
-rm -rf ~/.config/nvim
-ln -s $DOTFILES_DIR/config/nvim ~/.config/nvim
+## Neovim ##
+# save current nvim config
+if [ -d ~/.config/nvim ]; then
+  mv ~/.config/nvim ~/.config/nvim.old
+fi
 
-rm -rf ~/.vim
-ln -s $DOTFILES_DIR/vim/ ~/.vim
-
-rm -rf ~/.vimrc
-ln -s $DOTFILES_DIR/vimrc ~/.vimrc
-
-rm -rf ~/.shell
-ln -s $DOTFILES_DIR/shell ~/.shell
-rm -rf ~/.zsh
-ln -s $DOTFILES_DIR/zsh ~/.zsh
-
-rm -rf ~/.gitconfig
-ln -s $DOTFILES_DIR/gitconfig ~/.gitconfig
-rm -rf ~/.gitignore_global
-ln -s $DOTFILES_DIR/gitignore_global ~/.gitignore_global
-
-echo "Created symlinks"
-
-#==============
-# Install plug.nvim
-#==============
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-echo "Installed plug.nvim"
-
-#==============
-# Install oh-my-zsh
-#==============
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-rm -rf ~/.zshrc
-ln -s $DOTFILES_DIR/zshrc ~/.zshrc
+# create symlink to nvim config
+ln -s $DOTFILES_DIR/nvim ~/.config/nvim
 
